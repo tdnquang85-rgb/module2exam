735a4277774621f730a53b84cb31c82170c972a9
tdnquang85-rgb <tdnquang85@gmail.com>
2026-09-23 21:41:27 +0700

    5

--- src/App.java
+++ src/App.java
@@ -1,8 +1,5 @@
 import controller.BenhAnController;
 
-/**
- * Điểm khởi chạy ứng dụng Quản lý Bệnh án.
- */
 public class App {
     public static void main(String[] args) {
         BenhAnController controller = new BenhAnController();
